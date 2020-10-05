3b99e0a959d42dd74c0376894b0ebda8099197c4
Thomas Boileau <t-boileau@email.com>
2020-10-05 19:34:34 +0200

    Feature/control-product-added-to-cart (#53)
    * feat: control product added to cart
    
    * fix: update ProductVoter to check if token user is an User objbect
    
    * fix: delete dump

--- src/Security/Voter/ProductVoter.php
+++ src/Security/Voter/ProductVoter.php
@@ -2,8 +2,11 @@
 
 namespace App\Security\Voter;
 
+use App\Entity\CartItem;
+use App\Entity\Customer;
 use App\Entity\Producer;
 use App\Entity\Product;
+use App\Entity\User;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 
@@ -17,12 +20,14 @@ class ProductVoter extends Voter
 
     public const DELETE = "delete";
 
+    public const ADD_TO_CART = "add_to_cart";
+
     /**
      * @inheritDoc
      */
     protected function supports(string $attribute, $subject): bool
     {
-        return in_array($attribute, [self::UPDATE, self::DELETE]) && $subject instanceof Product;
+        return in_array($attribute, [self::UPDATE, self::DELETE, self::ADD_TO_CART]) && $subject instanceof Product;
     }
 
     /**
@@ -32,12 +37,32 @@ class ProductVoter extends Voter
     {
         $user = $token->getUser();
 
-        if (!$user instanceof Producer) {
+        if (!$user instanceof User) {
             return false;
         }
 
         /** @var Product $subject */
 
-        return $subject->getFarm() === $user->getFarm();
+        if ($attribute === self::ADD_TO_CART) {
+            return $user instanceof Customer && $this->voteOnAddToCart($user, $subject);
+        }
+
+        return $user instanceof Producer && $subject->getFarm() === $user->getFarm();
+    }
+
+    /**
+     * @param Customer $customer
+     * @param Product $product
+     * @return bool
+     */
+    private function voteOnAddToCart(Customer $customer, Product $product): bool
+    {
+        if ($customer->getCart()->count() === 0) {
+            return true;
+        }
+
+        return $customer->getCart()
+            ->map(fn (CartItem $cartItem) => $cartItem->getProduct()->getFarm())
+            ->contains($product->getFarm());
     }
 }
